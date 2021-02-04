b36cc28bc4c8bf1f4523ff11a0983744cd39ae2a
Haider Ali <1337neo@protonmail.com>
2021-02-04 15:00:34 +0530

    log to stdout

--- maps/settings.py
+++ maps/settings.py
@@ -125,3 +125,17 @@ STATIC_URL = '/static/'
 
 # CORS
 CORS_ALLOW_ALL_ORIGINS = True
+
+LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+            }
+        },
+        'root': {
+                'handlers': ['console'],
+                'level': 'WARNING',
+         },
+}
