4e3771ca7143847477407ddfa5e4d05f1f1167fd
Haider Ali <1337neo@protonmail.com>
2021-02-05 10:38:58 +0530

    Use xcacher to serve response

--- search/views.py
+++ search/views.py
@@ -2,8 +2,12 @@ from search.serializers import SearchSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from search.api.position_stack import PositionStack as map_api
-# from search.api.google import GoogleMap as map_api
+# from search.api.position_stack import PositionStack as map_api
+from search.api.google import GoogleMap as map_api
+from xcacher.engine import Engine
+from xcacher.errors import ItemNotFoundError
+
+cache = Engine(persistence_path='cache')
 
 
 def parse_error(errors):
@@ -11,25 +15,42 @@ def parse_error(errors):
     return ",".join(return_)
 
 
+def respond(data, err, cached):
+    response = {}
+    if err:
+        response['status'] = False
+        response['error'] = data
+    else:
+        response['status'] = True
+        response['results'] = data
+
+    response['cached'] = True if cached else False
+    return Response(response)
+
+
 class SearchList(APIView):
     def get(self, request, format=None):
         serializer = SearchSerializer(data=request.GET)
 
         if not serializer.is_valid():
-
             return Response({'success': False,
                              'error': parse_error(serializer.errors)},
                             status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
         cordinates = (serializer.validated_data['latitude'],
                       serializer.validated_data['longitude'])
+        key = f'{cordinates[0]}_{cordinates[1]}'
 
-        mapi = map_api(cordinates=cordinates)
-        mapi.work()
+        try:
+            item = cache.get(key)
+            return respond(item.data, False, True)
 
-        if not mapi.is_okay():
-            return Response({'success': False, 'error': mapi.get_error()},
-                            status=status.HTTP_503_SERVICE_UNAVAILABLE)
-        else:
-            return Response({'success': False, 'result': mapi.get_results()},
-                            status=status.HTTP_200_OK)
+        except ItemNotFoundError:
+            mapi = map_api(cordinates=cordinates)
+            mapi.work()
+
+            if not mapi.is_okay():
+                respond(mapi.get_error(), True, False)
+            else:
+                cache.store(key, mapi.get_results())
+                return respond(mapi.get_results(), False, False)
